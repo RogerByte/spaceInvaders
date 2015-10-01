d71435d0ca3f47e9930acb1cf3364aae68d920b6
Mario Roger Maldonado Toh <marioroger11@gmail.com>
2015-10-01 08:38:20 -0500

    Se añade clase Invader
    Se agrega clase Invader para generar un bloque de invasores

--- js/game.js
+++ js/game.js
@@ -5,7 +5,10 @@ function Game() {
     this.isRunning = false;
     this.lapseTime;
     this.pause = false;
-    this.velocityDesp = 100;
+    this.velocityDesp = 200;
+    this.counterInvader;
+    this.direction = false;
+
 
     this._construct = function(element) {
         /*
@@ -36,6 +39,17 @@ function Game() {
         this.spaceship = new Spaceship();
         this.spaceship._construct(this, 300, 600);
         this.listElements.push(this.spaceship);
+
+        this.counterInvader = 0;
+
+        for (var row = 0; row < 5; row++) {
+        	for(var column=0; column <12; column++){
+        		var invader = new Invader();
+        		invader._construct(this,100 + column*50, 50 + row*30);
+        		this.listElements.push(invader);
+        		this.counterInvader++;
+        	}
+        }
     }
 
     this.loop = function() {
@@ -44,14 +58,24 @@ function Game() {
             this.lapseTime = new Date().getTime();
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
             var elements = this.listElements.length;
-
+            // Mover elementos
             if (!this.pause) {
                 for (var i = 0; i < elements; i++) {
+                	//debugger
                     this.listElements[i].move(distance);
                 }
             }
+            // Dibujar elementos
             for (var i = 0; i < elements; i++) {
                 this.listElements[i].draw(this.context);
+            }
+            if(this.direction){
+            	var n=this.listElements.length;
+            	for(var i=0; i < n; i++){
+            		debugger
+            		this.listElements[i].behaviorOfMovement();
+            	}
+            	this.direction=false;
             }
 
             this.spaceship.setVelocityX(0);
@@ -60,6 +84,7 @@ function Game() {
             } else if(!this.leftDown && this.rightDown){
             	this.spaceship.setVelocityX(this.velocityDesp);
             }
+
 
         }
     }
@@ -95,6 +120,14 @@ function Game() {
         } else if (e.keyCode == 39) {
             this.rightDown = false;
         }
+    }
+
+    this.changeDirection = function() {
+    	this.direction = true;
+    }
+
+    this.over = function() {
+    	this.pause=true();
     }
 
 
---
+++ js/invader.js
@@ -0,0 +1,32 @@
+function Invader(){
+	this.velocityOfMovement = 50;
+	this.game;
+
+	this._construct = function(game, pointInitX, pointInitY) {
+		this._constructElement(pointInitX, pointInitY, 40, 25, "#00FF00");
+		this.game = game;
+		this.velocityX =-this.velocityOfMovement;
+	}
+
+    this.move = function(distance) {
+        if (this.velocityX < 0 && this.x < 10) {
+            this.game.changeDirection()
+        }
+        if (this.velocityX > 0 && this.x > 750) {
+            this.game.changeDirection();
+        }
+        this.moveElement(distance);
+    }
+
+    this.behaviorOfMovement = function(){
+    	this.velocityX = -this.velocityX;
+    	this.y+=10;
+
+    	if(this.y>570){
+    		this.game.over();
+    	}
+    }
+
+}
+
+Invader.prototype = new Element();
