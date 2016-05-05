c4eb8f489e1e7deb98510cf783f1fb32239c0b94
Roger Maldonado <marioroger11@gmail.com>
2016-05-05 16:47:52 -0500

    Se agrega lógica para disparos

--- js/game.js
+++ js/game.js
@@ -13,7 +13,7 @@ function Game() {
     this.loadPushButton = false;
     this.spacePush = false;
     this.arrowLeftPush = false;
-    this.arrowRightPush=false;
+    this.arrowRightPush = false;
     this.counterInvader;
     this.direction = false;
 
@@ -22,8 +22,20 @@ function Game() {
         this.canvas.width = 700;
         this.canvas.height = 650;
         this.canvas.style.backgroundColor = "black";
-
         this.context = this.canvas.getContext('2d');
+
+        this.panelMessage=document.createElement("div");
+        this.panelMessage.style.display="none";
+        this.panelMessage.style.position="absolute";
+        this.panelMessage.style.top=this.canvas.offsetTop;
+        this.panelMessage.style.left=this.canvas.offsetLeft;
+        this.panelMessage.style.width=this.canvas.width;
+        this.panelMessage.style.height=this.canvas.height;
+        this.panelMessage.style.paddingTop="250px";
+        this.panelMessage.style.textAlign="center";
+        this.panelMessage.style.color="yellow";
+        this.panelMessage.style.fontSize="x-large";
+        document.body.appendChild(this.panelMessage);
 
         this.createElement();
     }
@@ -42,17 +54,17 @@ function Game() {
         this.pause = false;
     }
 
-    this.initializeGame = function(){
-        this.listElements=[];
-        this.listElementsDeleted=[];
+    this.initializeGame = function() {
+        this.listElements = [];
+        this.listElementsDeleted = [];
         this.createElement();
 
         this.spacePush = false;
         this.arrowLeftPush = false;
-        this.arrowRightPush=false;
+        this.arrowRightPush = false;
 
-        this.lapseTime=new Date().getTime();
-        this.loadPushButton=false;
+        this.lapseTime = new Date().getTime();
+        this.loadPushButton = false;
     }
 
     this.createElement = function() {
@@ -63,12 +75,12 @@ function Game() {
         this.counterInvader = 0;
 
         for (var row = 0; row < 5; row++) {
-        	for(var column=0; column <12; column++){
-        		var invader = new Invader();
-        		invader._construct(this,100 + column*50, 50 + row*30);
-        		this.listElements.push(invader);
-        		this.counterInvader++;
-        	}
+            for (var column = 0; column < 12; column++) {
+                var invader = new Invader();
+                invader._construct(this, 100 + column * 50, 50 + row * 30);
+                this.listElements.push(invader);
+                this.counterInvader++;
+            }
         }
     }
 
@@ -78,6 +90,7 @@ function Game() {
             this.lapseTime = new Date().getTime();
             this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
             var elements = this.listElements.length;
+            var elementsDeleted = this.listElementsDeleted.length;
 
             for (var i = 0; i < elements; i++) {
                 this.listElements[i].move(distance);
@@ -87,21 +100,55 @@ function Game() {
                 this.listElements[i].draw(this.context);
             }
 
-            if(this.direction){
-            	var n=this.listElements.length;
-            	for(var i=0; i < n; i++){
-            		this.listElements[i].behaviorOfMovement();
-            	}
-            	this.direction=false;
+            for (var i = 0; i < elements - 1; i++) {
+                for (var j = i + 1; j < elements; j++) {
+                    var elementOne = this.listElements[i];
+                    var elementOther = this.listElements[j];
+                    if (elementOne.getTypeElement() != elementOther.getTypeElement() && elementOne.collision(elementOther)) {
+                        elementOne.collisionWith(elementOther);
+                        elementOther.collisionWith(elementOne);
+                    }
+                }
+            }
+
+            for (var i = 0; i < elementsDeleted; i++) {
+
+                for (var j = 0; j < elements; j++) {
+                    if (this.listElementsDeleted[i] == this.listElements[j]) {
+                        this.listElements.splice(j, 1);
+                        break;
+                    }
+                }
+            }
+
+            this.listaEntidadesEliminar = [];
+
+            if (this.direction) {
+                for (var i = 0; i < elements; i++) {
+                    this.listElements[i].behaviorOfMovement();
+                }
+                this.direction = false;
+            }
+
+            if (this.loadPushButton) {
+                this.panelMessage.style.display = "block";
+            } else {
+                this.panelMessage.style.display = "none";
             }
 
             this.spaceship.setVelocityX(0);
-            if(this.leftDown && !this.rightDown){
-            	this.spaceship.setVelocityX(-this.velocityDesp);
-            } else if(!this.leftDown && this.rightDown){
-            	this.spaceship.setVelocityX(this.velocityDesp);
+
+            if (this.leftDown && !this.rightDown) {
+                this.spaceship.setVelocityX(-this.velocityDesp);
+            } else if (!this.leftDown && this.rightDown) {
+                this.spaceship.setVelocityX(this.velocityDesp);
             }
-        } else{
+
+            if (this.spacePush) {
+                this.shooting();
+            }
+
+        } else {
             this.lapseTime = new Date().getTime();
             this.isRunning = true;
         }
@@ -111,8 +158,13 @@ function Game() {
         e.preventDefault();
         var key = e.keyCode;
 
-        if(DEBUGGER){
-            console.info(e.code + ' : ' + e.keyCode)
+        if (DEBUGGER) {
+            console.info('keydown ' + e.code + ' : ' + e.keyCode)
+        }
+
+        if (this.loadPushButton && key == 13) {
+            this.initializeGame();
+            return;
         }
 
         if (this.pause && key == 13) {
@@ -126,84 +178,124 @@ function Game() {
         } else if (key == 39) {
             this.rightDown = true;
         }
+
+        if (key == 32) {
+            this.spacePush = true;
+        }
+
     }
 
     this.onkeyup = function(e) {
         e.preventDefault();
         var key = e.keyCode;
-
-        if(DEBUGGER){
-            console.info(e.code + ' : ' + e.keyCode)
+        if (DEBUGGER) {
+            console.info('keyup ' + e.code + ' : ' + e.keyCode)
         }
 
         if (key == 37) {
             this.leftDown = false;
         } else if (e.keyCode == 39) {
             this.rightDown = false;
+        }else if (key == 32) {
+            this.spacePush = false;
         }
 
         if (key == 80) {
             game.applyPause();
         }
+
     }
 
     this.changeDirection = function() {
-    	this.direction = true;
+        this.direction = true;
     }
 
     this.over = function() {
-    	this.pause=true;
+        this.pause = true;
     }
 
-    this.applyPause = function(){
-        if(this.pause){
-            this.pause=false;
-        }else{
+    this.applyPause = function() {
+        if (this.pause) {
+            this.pause = false;
+        } else {
             this.isRunning = false;
-            this.pause=true;
+            this.pause = true;
         }
     }
 
-    this.setMessage= function(text){
+    this.setMessage = function(text) {
         var text = text || "PAUSE";
         this.context.font = "30px monospace";
-        this.context.fillStyle  = "red";
-        this.context.textAlign="center";
-        this.context.fillText(text,330,30);
+        this.context.fillStyle = "red";
+        this.context.textAlign = "center";
+        this.context.fillText(text, 330, 30);
 
         this.getHelp();
     }
 
-
-    this.getHelp = function(){
+    this.getHelp = function() {
         var self = this.context;
 
-        self.fillStyle  = "black";
-        self.fillRect(90,50,500,200);
+        self.fillStyle = "black";
+        self.fillRect(90, 50, 500, 200);
 
-        self.fillStyle  = "white";
+        self.fillStyle = "white";
         self.strokeStyle = 'blue'
-        self.strokeRect(90,50,500,200);
+        self.strokeRect(90, 50, 500, 200);
 
         self.font = "30px monospace";
-        self.fillText("Controls", 330,85);
+        self.fillText("Controls", 330, 85);
 
         self.font = "20px monospace";
-        self.fillText("Arrow Left : Move to Left", 325,120);
-        self.fillText("Arrow Right : Move to Right", 325,140);
-        self.fillText("P : Pause", 335,160);
+        self.fillText("Arrow Left : Move to Left", 325, 120);
+        self.fillText("Arrow Right : Move to Right", 325, 140);
+        self.fillText("P : Pause", 335, 160);
 
-        self.fillText("Have Fun! :)", 330,200);
+        self.fillText("Have Fun! :)", 330, 200);
 
     }
 
-    this.deleteElement = function(){
-        console.log('delete Element')
+    this.deleteElement = function(element) {
+        this.listElementsDeleted.push(element)
     }
 
-    this.notificateDestroy=function(){
+    this.notificateDestroy = function() {
+        this.counterInvader--;
+        if (this.counterInvader == 0) {
+            this.notificateWin();
+        } else {
+            var numElements = this.listElements.length;
+            for (var i = 0; i < numElements; i++) {
+                if (this.listElements[i] instanceof Invader) {
+                    this.listElements[i].setVelocityX(this.listElements[i].getVelocityX() * 1.02);
+
+                }
+            }
+        }
         console.log('Element destroyed!')
     }
 
-}
+    this.shooting = function() {
+        if(DEBUGGER){
+            console.log('Disparando')
+        }
 
+        if (this.loadPushButton) {
+            return;
+        }
+
+        var time = new Date().getTime();
+        if (time - this.lastShoot < this.shootInterval) {
+            return;
+        }
+
+        this.lastShoot = time;
+
+        var shoot = new Bullet();
+        shoot._construct(this, this.spaceship.getPositionX() + 19, this.spaceship.getPositionY());
+
+        this.listElements.push(shoot);
+    }
+
+
+}
